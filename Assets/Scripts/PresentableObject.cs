0baf8c51b99d64a6a69caeb00e30efde2999ae47
cm600286 <91765679+cm600286@users.noreply.github.com>
2022-04-02 01:54:14 -0700

    PresentableObject
    Click on the object (like a letter) to expand it on the screen. Then click and drag to scroll up or down. Press space and click to place the object back.
    
    Currently, the space is only temporary - I need to move the listener in the Clickable object class to OnMouseUp in order to implement a timing system where long presses will trigger scrolling. When scrolling, the mouse cursor will be locked to the center. There is a fix, but it will likely break the game on non-windows computers.

---
+++ Assets/Scripts/PresentableObject.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PresentableObject : MonoBehaviour
+{
+    [SerializeField] public float zoomSpeed;
+
+    private bool onScreen;
+    private bool mouseDown;
+    private Vector3 origPosition;
+    private Vector3 camPosition;
+    private float width;
+    private float stepAngle;
+
+    void Start()
+    {
+        onScreen = false;
+        mouseDown = false;
+        width = this.GetComponent<Renderer>().bounds.size.x;
+        camPosition = Camera.main.transform.position + Vector3.forward * width;
+        origPosition = this.transform.position;
+        stepAngle = 90 / Vector3.Distance(camPosition, origPosition) * zoomSpeed * 1.25f;
+    }
+
+    public void Update()
+    {
+        if (mouseDown)
+        {
+            this.transform.position +=
+                Vector3.up * Input.GetAxis("Mouse Y") / Screen.height * 10 * width;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        mouseDown = onScreen;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void OnMouseUp()
+    {
+        mouseDown = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+    public void present()
+    {
+        if (!onScreen || Input.GetKey(KeyCode.Space))
+        {
+            StartCoroutine(rotateStep(!onScreen));
+            StartCoroutine(zoomStep(!onScreen));
+        }
+    }
+
+    public IEnumerator zoomStep(bool expand)
+    {
+        Vector3 target = expand ? camPosition : origPosition;
+        while (Vector3.Distance(target, this.transform.position) > zoomSpeed)
+        {
+            this.transform.position += (Vector3.Normalize(target - this.transform.position) * zoomSpeed);
+            yield return null;
+        }
+        onScreen = expand;
+        this.transform.position = target;
+
+    }
+    public IEnumerator rotateStep(bool expand)
+    {
+        for (float i = 0; i < 90; i += stepAngle)
+        {
+            this.transform.rotation = Quaternion.Euler(expand ? -i: i - 90, 0, 0); ;
+            yield return null;
+        }
+        this.transform.rotation = Quaternion.Euler(expand ? -90 : 0, 0, 0); ;
+    }
+}
